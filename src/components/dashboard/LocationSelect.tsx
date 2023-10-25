9adb2cd500bd1d76278a9f2e317c5c4812bf0599
Joost Schuur <jschuur@jschuur.com>
2023-10-25 13:44:14 +0800

    Update URL to default location

--- src/components/dashboard/LocationSelect.tsx
+++ src/components/dashboard/LocationSelect.tsx
@@ -2,14 +2,23 @@
 
 import { Select, SelectItem } from '@tremor/react';
 import { useQueryState } from 'next-usequerystate';
+import { useMemo } from 'react';
 
 import { useLocations } from '@/hooks/useLocations';
+import { useEffect } from 'react';
 
 export function LocationSelect() {
   const [location, setLocation] = useQueryState('location');
-  const { data, isError, isFetching, error } = useLocations();
+  const { data, isError, isFetching } = useLocations();
 
-  const locations = data?.locations || [];
+  const locations = useMemo(() => data?.locations || [], [data]);
+
+  useEffect(() => {
+    if (locations && locations?.length > 0 && !location) {
+      setLocation(locations[0].slug);
+    }
+  }, [location, locations, setLocation]);
+
   const hasLocations = Boolean(locations && locations.length > 0);
   const placeholder = hasLocations
     ? 'Select a location'
@@ -23,8 +32,8 @@ export function LocationSelect() {
     <div className='max-w-sm mx-auto space-y-6'>
       <Select
         className='w-full'
-        defaultValue={location || locations?.[0]?.slug}
         enableClear={false}
+        value={location || undefined}
         onValueChange={setLocation}
         placeholder={placeholder}
         disabled={locations.length === 0}
